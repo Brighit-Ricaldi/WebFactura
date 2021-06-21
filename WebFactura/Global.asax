81d0f993768930dd5aa7fe4a172a7029a1126614
Brighit-Ricaldi <brighit.ricaldi@tecsup.edu.pe>
2021-06-21 17:21:25 -0500

    commit

---
+++ WebFactura/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFactura.MvcApplication" Language="C#" %>
---
+++ WebFactura/Models/Clientes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFactura.Models
+{
+    public class Clientes
+    {
+    }
+}
